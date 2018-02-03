5f6536b7ed935c345b8bbe34fd47ccc009156a4e
Daebak-Kijeong <Daebak-kijeong@daum.net>
2018-02-03 16:40:10 +0900

    update code

---
+++ hackerrank-code.py
@@ -0,0 +1,17 @@
+##bon-appetit
+import sys
+
+def bonAppetit(n, k, b, ar):
+
+
+
+    return
+# Complete this function
+
+n, k = input().strip().split(' ')
+n, k = [int(n), int(k)]
+ar = list(map(int, input().strip().split(' ')))
+b = int(input().strip())
+result = bonAppetit(n, k, b, ar)
+print(result)
+
